22a29eb221b160fe813867f722d4c98e737b0f26
Ibrahim Metwaly <ibrahim.metwaly@polteq.com>
2025-06-16 17:53:53 +0200

    some optimimlization

--- src/test/java/tests/TestShopScenario.java
+++ src/test/java/tests/TestShopScenario.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import tests.lib.DriverFactory;
+import tests.pages.AuthenticationPage;
+import tests.pages.HeaderPage;
 
 import java.time.Duration;
 
@@ -17,29 +19,21 @@ public class TestShopScenario {
 
    protected WebDriver driver;
    protected WebDriverWait wait;
+   protected HeaderPage headerPage;
+   protected AuthenticationPage authenticationPage;
 
     @BeforeMethod
     public void setUp() {
         driver = DriverFactory.createDriver(DriverFactory.BrowserType.FIREFOX);
+        headerPage = new HeaderPage(driver);
+        authenticationPage = new AuthenticationPage(driver);
         driver.get("https://greatshop.polteq-testing.com/");
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //overruled implicitly
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //standard on all
     }
 
     @AfterMethod
     public void tearDown() {
         driver.quit();
     }
-
-    protected void logIn(String email, String pwd){
-        driver.findElement(By.className("login")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email"))).sendKeys(email);
-        driver.findElement(By.id("passwd")).sendKeys(pwd);
-        driver.findElement(By.id("SubmitLogin")).click();
-
-        //testNG
-        WebElement logoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("logout")));
-        assertTrue(logoutButton.isDisplayed(), "Log out button is not visible");
-        System.out.print("Log out button is present, so user is logged in");
-    }
-
 }
